0017bc877f35c5badc33de489d743a0759cfc1c8
Victor Goff <keeperotphones@gmail.com>
2010-05-31 08:30:14 -0500

    added extra credit failure on year 0, and modified general test, it doesn't require 'testutils'

--- lib/leapyear.rb
+++ lib/leapyear.rb
@@ -1,4 +1,10 @@
-
 def leap_year?(year)
-  year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)
+  case year
+  when 0
+    raise ArgumentError, "Year 0 doesn't exist in Gregorian or Julian Calendars." 
+  else
+    year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)
+  end
 end
+
+
--- test/test_leapyear.rb
+++ test/test_leapyear.rb
@@ -1,5 +1,4 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "../lib", "leapyear"))
-require File.expand_path(File.join(File.dirname(__FILE__), "../test", "testutils"))
 
 require 'test/unit'
 
@@ -7,16 +6,28 @@ class TestLeapYear < Test::Unit::TestCase
   include TestUtils
   
   def setup
-    @tests = [
-      [0, 400, 444, 1111, 1999, 1900, 2000, 2004, 2010, 2020], 
-      [true, true, true, false, false, false, true, true, false, true]
-    ]
-   
+    @value_and_expected = {-100 => false, 400 => true, 1111 => false, 1999 => false, 2000 => true,
+                            100 => false, 2002 => false, 2004 => true, 2010 => false, 2020 => true } 
   end 
  
   def test_leap_year   
-    @tests[0].each_with_index do |year, i|
-      assert_equal @tests[1][i], leap_year?(year), top_level_method_call_format(:leap_year?, year)
+    @value_and_expected.each_pair do |value, expected| 
+      assert_equal leap_year?(value), expected 
     end
   end
+
+  def test_leap_year_edge_case_year_0_does_not_exist_so_raise_argument_error
+    # This doesn't use the setup at all  It simply tests one edge case.
+    # Year 0 doesn't exist in Julian or Gregorian Calendars... so my method should give an error.
+    assert_raise( ArgumentError) { leap_year?(0) }
+  end
+
+  def test_other_edge_cases
+    # Other edge cases are years 100, 300, 500, etc. as these years were all leap years, based on the simpler
+    # rules (The year only had to be divisble by 4).  This means that technically, even 100, and -100 should be leap year.
+    # Depending on the year of adoption of leap year, and adoption of the Gregorian calendar, other countries have differing
+    # edge case years.
+  end
 end
+
+
