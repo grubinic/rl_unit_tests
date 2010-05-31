0d3d74f0a02e479d5aa0cad72304a6797381709f
grubinic <gorana.rubinic@gmail.com>
2010-06-01 00:45:48 +0200

    refactored test data

--- test/test_convert_fahr_to_cel.rb
+++ test/test_convert_fahr_to_cel.rb
@@ -9,17 +9,17 @@ class TestConvertFahrToCel < Test::Unit::TestCase
   include TestUtils
   
   def setup
-    @tests = [
-      [-508, -459.67, -58.95400, 0, 32, 77.72, 100.76,  212, 523.67], 
-      [-300, -273.15, -50.53, -17.7777778, 0, 25.4, 38.2, 100, 273.15]
-    ]
+    @temp_fahr_and_expected = {-508 => -300, -459.67 => -273.15, -58.95400 => -50.53, 0 => -17.7777778,
+      32 => 0, 77.72 => 25.4, 100.76 => 38.2, 212 => 100, 523.67 => 273.15 }      
   end
   
   def test_convert    
-    @tests[0].each_with_index do |temp_fahr, i|
-      assert_equal @tests[1][i].round(2), convert(temp_fahr).round(2), top_level_method_call_format(:convert, temp_fahr)
+    @temp_fahr_and_expected.each do |temp_fahr, expected_temp_cel|
+      assert_equal expected_temp_cel.round(2), convert(temp_fahr).round(2), top_level_method_call_format(:convert, temp_fahr)
     end
   end
+  
+  #TODO values below absolute zero - what's the expected result?
 end
 
 
--- test/test_gameboard.rb
+++ test/test_gameboard.rb
@@ -14,18 +14,14 @@ class TestGameboard < Test::Unit::TestCase
   include TestUtils
   
   def test_game_board    
-    @tests[:locations].each_with_index do |location_cells, location_cells_index|
-      
+    @tests.each do |location_cells, guesses, expected_results|      
       game_board = GameBoard.new
       game_board.set_locations_cells(location_cells)
-      guesses = @tests[:guesses][location_cells_index]
-      
       guesses.each_with_index do |user_guess, guess_index|
-        assert_equal @tests[:results][location_cells_index][guess_index].test_game_board_format, 
+        assert_equal expected_results[guess_index].test_game_board_format, 
         game_board.check_yourself(user_guess).test_game_board_format,
         format_assert_msg(game_board, location_cells, guesses, guess_index, user_guess)         
-      end      
-    
+      end
     end
   end
   
@@ -38,9 +34,16 @@ class TestGameboard < Test::Unit::TestCase
   end
   
   def setup
-    @tests = { :locations => [[0,1,2], [1,2,3], [2,3,4], [3,4,5], [4,5,6]],
-      :guesses => [[2,3,1,4,5,6,7,"0"], [3,3,3,0,2,1],[2,"X",3,4], [0,1,2,3,4,4,5], [6,5,3,5,6,4]],
-      :results => [[:Hit,:Miss,:Hit,:Miss,:Miss,:Miss,:Miss,:Kill], [:Hit,:Hit,:Hit,:Miss,:Hit,:Kill],
-      [:Hit,:Miss,:Hit,:Kill], [:Miss,:Miss,:Miss,:Hit,:Hit,:Hit,:Kill],  [:Hit,:Hit,:Miss,:Hit,:Hit,:Kill]] }
+    @tests = [
+      [[0,1,2], [2,3,1,4,5,6,7,"0"], [:Hit,:Miss,:Hit,:Miss,:Miss,:Miss,:Miss,:Kill]],
+      [[1,2,3], [3,3,3,0,2,1], [:Hit,:Hit,:Hit,:Miss,:Hit,:Kill]],
+      [[2,3,4], [2,"X",3,4], [:Hit,:Miss,:Hit,:Kill]],
+      [[3,4,5], [0,1,2,3,4,4,5], [:Miss,:Miss,:Miss,:Hit,:Hit,:Hit,:Kill]],
+      [[4,5,6], [6,5,3,5,6,4], [:Hit,:Hit,:Miss,:Hit,:Hit,:Kill]]
+    ]
   end 
 end
+
+#TODO refactor to specific tests, for example: test_same_guess_three_times, test_guess_after_kill, 
+#test_zero_guess, test_invalid_guess ?
+#TODO refactor test data model ?
